1b2fdba22eff91fa45ea5ccdb1c06d47c678bca3
XieNoDesire <2720289676@qq.com>
2025-04-11 07:27:23 +0800

    完成多媒体页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import pinia from '@/stores/index.js'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
 import '@/assets/main.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
 
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
